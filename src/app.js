661ec4a6ceefa2a7e8b5cd1886cdd7a49abf70da
mike_kotikov <kmikecoder@gmail.com>
2019-05-14 14:38:59 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.PureComponent {
+  render() {
+    return <h1>The page has rendered!</h1>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html"
+    })
+  ]
+};
