2860afa6f0a820c7ed44f8cdf847197d1a1f2d67
mike_kotikov <kmikecoder@gmail.com>
2019-05-17 17:41:08 +0300

    create array with navigation routes

---
+++ src/constants/header/links.js
@@ -0,0 +1,7 @@
+export default [
+  { path: '/', name: 'Home' },
+  { path: '/airplanes', name: 'Airplanes' },
+  { path: '/airports', name: 'Airports' },
+  { path: '/flights', name: 'Flights' },
+  { path: '/auth', name: 'Sign in / Sign out' }
+];
