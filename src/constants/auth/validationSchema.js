9dee470b3ef6641fd434dfe4b177abca1171a612
mike_kotikov <kmikecoder@gmail.com>
2019-05-17 15:16:35 +0300

    disable check for email and password max length

--- src/constants/auth/validationSchema.js
+++ src/constants/auth/validationSchema.js
@@ -1,14 +1,13 @@
 import * as yup from 'yup';
 
 import infoText from '../infoText';
-import regex from './regex';
+import regex from '../regex';
 
 const validateEmail = yup.object().shape({
   email: yup
     .string()
     .required(infoText.requiredEmail)
     .trim()
-    .max(30, infoText.long)
     .test('isEmailValid', infoText.invalidEmail, value =>
       regex.email.test(value)
     )
@@ -20,10 +19,6 @@ const validatePassword = yup.object().shape({
     .required(infoText.requiredPassword)
     .trim()
     .min(6, infoText.short)
-    .max(16, infoText.long)
-    .test('isPasswordValid', infoText.invalidPassword, value =>
-      regex.password.test(value)
-    )
 });
 
 const validationSchema = {
