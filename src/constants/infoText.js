108d1f5b9f3de2455f29d0644f567e252d63a138
mike_kotikov <kmikecoder@gmail.com>
2019-05-17 14:03:43 +0300

    changed hint text for invalid password enter

--- src/constants/infoText.js
+++ src/constants/infoText.js
@@ -2,7 +2,8 @@ const infoText = {
   requiredEmail: 'Email is required.',
   requiredPassword: 'Password is required.',
   invalidEmail: 'Please, check your email for being correct.',
-  invalidPassword: 'Password should not contain symbols.',
+  invalidPassword:
+    'Password should have at least one digit. It should not contain symbols.',
   short: 'Too short!',
   long: 'Too long!'
 };
