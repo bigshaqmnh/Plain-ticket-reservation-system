06d893b7f742d36d72eaaa5da9a8d15000f7db3c
bigshaqmnh <kmikecoder@gmail.com>
2019-05-18 20:00:28 +0300

    add ids

--- src/constants/header/links.js
+++ src/constants/header/links.js
@@ -1,7 +1,7 @@
 export default [
-  { path: '/', name: 'Home' },
-  { path: '/airplanes', name: 'Airplanes' },
-  { path: '/airports', name: 'Airports' },
-  { path: '/flights', name: 'Flights' },
-  { path: '/auth', name: 'Sign in / Sign out' }
+  { id: 1, path: '/', name: 'Home' },
+  { id: 2, path: '/airplanes', name: 'Airplanes' },
+  { id: 3, path: '/airports', name: 'Airports' },
+  { id: 4, path: '/flights', name: 'Flights' },
+  { id: 5, path: '/auth', name: 'Sign in / Sign out' }
 ];
