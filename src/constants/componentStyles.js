464035cfa9d88fb5d213257d9c0479ef3e321266
bigshaqmnh <37017407+bigshaqmnh@users.noreply.github.com>
2019-05-18 20:20:52 +0300

    rename and move

--- src/constants/componentStyles.js
+++ src/constants/componentStyles.js
@@ -1,7 +1,8 @@
-Unstaged changes after reset:
-D	src/constants/button/buttonVariants.js
-M	src/constants/componentStyles.js
-M	src/constants/header/links.js
-D	src/constants/infoText.js
-M	src/containers/auth/index.js
-M	src/containers/header/index.js
+const componentStyles = {
+  default: 'primary',
+  success: 'success',
+  warning: 'warning',
+  error: 'danger'
+};
+
+export default componentStyles;
