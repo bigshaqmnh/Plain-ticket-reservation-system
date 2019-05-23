23c12794021e5c828ae47a6f0a7717fc0a913d25
mike_kotikov <kmikecoder@gmail.com>
2019-05-23 10:34:09 +0300

    delete useless links.js

--- src/constants/header/links.js
+++
@@ -1,7 +0,0 @@
-export default [
-  { id: 1, path: '/', name: 'Home' },
-  { id: 2, path: '/airplanes', name: 'Airplanes' },
-  { id: 3, path: '/airports', name: 'Airports' },
-  { id: 4, path: '/flights', name: 'Flights' },
-  { id: 5, path: '/auth', name: 'Sign in / Sign out' }
-];
