e424f68171f3c58bde4cccac8042fc78b7ebae0f
mike_kotikov <kmikecoder@gmail.com>
2019-05-30 11:02:16 +0300

    update imports

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const c = require('./controllerHandler');
-const controller = require('../controllers/auth');
+const controllerHandler = require('./controllerHandler');
+const authController = require('../controllers/auth');
 
-router.post('/login', c(controller.logIn, (req, res, next) => [req.body.email, req.body.password]));
+router.post('/login', controllerHandler(authController.logIn, (req, res, next) => [req.body.email, req.body.password]));
 
 module.exports = router;
