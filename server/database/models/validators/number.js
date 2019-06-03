5e8e8f39cf64d330f27e046e3acbc71cb8f3320e
mike_kotikov <kmikecoder@gmail.com>
2019-06-03 13:50:41 +0300

    add validation rule for decimals

--- server/database/models/validators/number.js
+++ server/database/models/validators/number.js
@@ -1,4 +1,5 @@
 const integer = { isInt: true };
 const float = { isFloat: true };
+const decimal = { isDecimal: true };
 
-module.exports = { integer, float };
+module.exports = { integer, float, decimal };
