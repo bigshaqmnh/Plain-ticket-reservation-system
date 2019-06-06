6055c2e998567e6a9210822e35c766a3ef8c8225
mike_kotikov <kmikecoder@gmail.com>
2019-06-06 18:36:28 +0300

    improve seats generator

--- server/database/seeders/20190604084139-seat.js
+++ server/database/seeders/20190604084139-seat.js
@@ -1,26 +1,33 @@
 'use strict';
 
-const getRandAirplaneId = () => Math.round(Math.random() * (6 - 1) + 1);
+const seat = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+const seat_index = [3, 5, 7, 8];
+
+const getRandMaxRow = () => Math.round(Math.random() * (40 - 20) + 20);
+const getRandMaxIndex = () => Math.round(Math.random() * 3);
 const getRandSeatTypeId = () => Math.round(Math.random() * (2 - 1) + 1);
-const getRandFloor = () => Math.round(Math.random() * (2 - 1) + 1);
+const getRandMaxFloor = () => Math.round(Math.random() * (2 - 1) + 1);
 const getRandIsBooked = () => (Math.random() > 0.6 ? true : false);
 
 const generateSeats = () => {
-  const indexes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
   let seats = [];
 
-  for (let row = 1, max_row = 40; row <= max_row; ++row) {
-    for (let index = 0, max_index = indexes.length; index < max_index; ++index) {
-      seats.push({
-        row,
-        seat: indexes[index],
-        floor: getRandFloor(),
-        airplaneId: getRandAirplaneId(),
-        seatTypeId: getRandSeatTypeId(),
-        isBooked: getRandIsBooked(),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      });
+  for (let airplaneId = 1, maxAirplaneId = 6; airplaneId <= maxAirplaneId; ++airplaneId) {
+    for (let floor = 1, max_floor = getRandMaxFloor(); floor <= max_floor; ++floor) {
+      for (let row = 1, max_row = getRandMaxRow(); row <= max_row; ++row) {
+        for (let index = 0, max_index = seat_index[getRandMaxIndex()]; index <= max_index; ++index) {
+          seats.push({
+            row,
+            seat: seat[index],
+            floor,
+            airplaneId,
+            seatTypeId: getRandSeatTypeId(),
+            isBooked: getRandIsBooked(),
+            createdAt: new Date(),
+            updatedAt: new Date()
+          });
+        }
+      }
     }
   }
 
