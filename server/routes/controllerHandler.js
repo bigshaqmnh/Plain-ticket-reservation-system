4a5b2411f0460082bd58aa6b7bf75913dca3af09
mike_kotikov <kmikecoder@gmail.com>
2019-06-07 18:48:00 +0300

    refactor boundParams and result sending

--- server/routes/controllerHandler.js
+++ server/routes/controllerHandler.js
@@ -1,12 +1,18 @@
 const statusCode = require('http-status-codes');
 
 const controllerHandler = (promise, params) => async (req, res, next) => {
-  const boundParams = params ? params(req, res, next) : [];
+  const boundParams = params(req, res, next);
 
   try {
-    const { err, data } = await promise(...boundParams);
+    const { err, data } = await promise(boundParams);
 
-    return err ? res.status(data.status).json(data.message) : res.status(statusCode.OK).json(data);
+    if (err && data) {
+      return res.status(data.status).json(data.message);
+    } else if (data) {
+      return res.status(statusCode.OK).json(data);
+    }
+
+    return res.status(statusCode.OK);
   } catch (error) {
     return res.status(statusCode.INTERNAL_SERVER_ERROR).json(error);
   }
