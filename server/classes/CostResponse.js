4c07f784a05e12ea0c501d84999053f2fcb052ea
mike_kotikov <kmikecoder@gmail.com>
2019-06-11 13:43:37 +0300

    create response class

---
+++ server/classes/CostResponse.js
@@ -0,0 +1,16 @@
+class CostResponse {
+  constructor(err = false, data = null) {
+    this._err = err;
+    this._data = data;
+  }
+
+  get err() {
+    return this._err;
+  }
+
+  get data() {
+    return this._data;
+  }
+}
+
+module.exports = CostResponse;
