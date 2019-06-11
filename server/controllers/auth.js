fc25cdf07598a219285fa52a7f2293c6a7b4ea0b
mike_kotikov <kmikecoder@gmail.com>
2019-06-11 16:20:06 +0300

    create signUp function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,27 +1,55 @@
 const userService = require('../services/user');
 const AuthResponse = require('../classes/AuthResponse');
-const error = require('../constants/errors');
+const { reqError, dbError } = require('../constants/errors');
 
 const logIn = async ({ email, password }) => {
-  const user = await userService.findByEmail(email);
+  try {
+    const user = await userService.findByEmail(email);
 
-  if (user) {
-    const passwordsMatch = await userService.comparePasswords(password, user.passwordHash);
+    if (user) {
+      const passwordsMatch = await userService.comparePasswords(password, user.passwordHash);
 
-    if (passwordsMatch) {
-      const { id, username } = user;
-      const payload = {
-        id,
-        username
-      };
+      if (passwordsMatch) {
+        const { id, username } = user;
+        const payload = {
+          id,
+          username
+        };
 
-      const token = await userService.generateToken(payload);
+        const token = await userService.generateToken(payload);
 
-      return new AuthResponse(false, `Bearer ${token}`);
+        return new AuthResponse(false, `Bearer ${token}`);
+      }
     }
-  }
 
-  return new AuthResponse(true, error.logIn);
+    return new AuthResponse(true, reqError.logIn);
+  } catch (err) {
+    return new AuthResponse(true, dbError.logIn);
+  }
 };
 
-module.exports = { logIn };
+const signUp = async ({ username, email, password }) => {
+  try {
+    const user = await userService.checkIfExists(email);
+
+    if (user) {
+      return new AuthResponse(true, reqError.singUp);
+    }
+
+    const passwordHash = await userService.hashPassword(password);
+
+    const newUser = {
+      username,
+      email,
+      passwordHash
+    };
+
+    await userService.add(newUser);
+
+    return new AuthResponse();
+  } catch (err) {
+    return new AuthResponse(true, dbError.signUp);
+  }
+};
+
+module.exports = { logIn, signUp };
