ac8eaeaedce69aebadac0ae0b9e376f031bd572c
mike_kotikov <kmikecoder@gmail.com>
2019-06-11 13:55:09 +0300

    add signUp route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,4 +6,6 @@ const authController = require('../controllers/auth');
 
 router.post('/login', controllerHandler(authController.logIn, (req, res, next) => req.body));
 
+router.post('/signUp', controllerHandler(authController.signUp, (req, res, next) => req.body));
+
 module.exports = router;
