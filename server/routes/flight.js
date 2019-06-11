811843f1bcff610ede54e704c7cb964771d51dac
mike_kotikov <kmikecoder@gmail.com>
2019-06-11 13:54:44 +0300

    add update route

--- server/routes/flight.js
+++ server/routes/flight.js
@@ -3,18 +3,24 @@ var router = express.Router();
 const passport = require('passport');
 
 const controllerHandler = require('./controllerHandler');
-const flightsController = require('../controllers/flight');
+const flightController = require('../controllers/flight');
 
-router.get('/', controllerHandler(flightsController.getFlights, (req, res, next) => req.query));
+router.get('/all', controllerHandler(flightController.getAll, (req, res, next) => req.query));
 
-router.get('/:flightId', controllerHandler(flightsController.getFlightById, (req, res, next) => req.params));
+router.get('/', controllerHandler(flightController.getAllByParams, (req, res, next) => req.body));
 
-router.get('/:flightId', controllerHandler(flightsController.getFlightById, (req, res, next) => req.params));
+router.get('/:flightId', controllerHandler(flightController.getById, (req, res, next) => req.params));
 
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
-  controllerHandler(flightsController.addFlight, (req, res, next) => req.body)
+  controllerHandler(flightController.add, (req, res, next) => req.body)
+);
+
+router.post(
+  '/:flightId',
+  passport.authenticate('jwt', { session: false }),
+  controllerHandler(flightController.update, (req, res, next) => ({ id: req.params.flightId, flight: req.body }))
 );
 
 module.exports = router;
