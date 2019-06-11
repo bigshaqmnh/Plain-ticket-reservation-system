a160e0e8c69a6becfe1dd699b40828960eb3cdac
mike_kotikov <kmikecoder@gmail.com>
2019-06-11 13:45:13 +0300

    add error messages for different scenarious

--- server/constants/errors.js
+++ server/constants/errors.js
@@ -1,12 +1,15 @@
 const statusCode = require('http-status-codes');
 
 const reqError = {
-  logIn: { status: statusCode.UNAUTHORIZED, message: 'Unable to log in. Check your credentials.' }
+  logIn: { status: statusCode.UNAUTHORIZED, message: 'User not found. Check your credentials.' },
+  singUp: { status: statusCode.UNAUTHORIZED, message: 'User with such email already exists.' }
 };
 
 const dbError = {
+  logIn: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to log in.' },
   get: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to get data.' },
-  create: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to create new instance.' }
+  create: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to create new instance.' },
+  update: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to update the instance.' }
 };
 
 module.exports = { reqError, dbError };
