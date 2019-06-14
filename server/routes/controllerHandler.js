a97ad0f1d0471e7f376e3c5a1c2bc0e859c9a628
mike_kotikov <kmikecoder@gmail.com>
2019-06-14 15:46:38 +0300

    change error handling logic

--- server/routes/controllerHandler.js
+++ server/routes/controllerHandler.js
@@ -1,20 +1,22 @@
-const statusCode = require('http-status-codes');
+const status = require('../constants/status');
+const CustomError = require('../classes/CustomError');
 
-const controllerHandler = (promise, params) => async (req, res, next) => {
+const controllerHandler = (controller, params) => async (req, res, next) => {
   const boundParams = params(req, res, next);
+  const method = req.method;
 
   try {
-    const { err, data } = await promise(boundParams);
+    const data = await controller(boundParams);
 
-    if (err && data) {
-      return res.status(data.status).json(data.message);
-    } else if (data) {
-      return res.status(statusCode.OK).json(data);
+    if (data instanceof CustomError) {
+      console.error('ERROR: ', data.message);
+      return data.dbError ? res.sendStatus(status.fail.DB) : res.sendStatus(status.fail[method]);
     }
 
-    return res.sendStatus(statusCode.OK);
-  } catch (error) {
-    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(error);
+    return data ? res.status(status.success[method]).json(data) : res.sendStatus(status.success[method]);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(status.fail.SERVER);
   }
 };
 
