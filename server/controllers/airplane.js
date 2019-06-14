2c30fcb988067624d1306022c80fc06f599796d8
mike_kotikov <kmikecoder@gmail.com>
2019-06-14 15:49:37 +0300

    refactor error handling

--- server/controllers/airplane.js
+++ server/controllers/airplane.js
@@ -1,14 +1,13 @@
 const airplaneService = require('../services/airplane');
-const AirplaneResponse = require('../classes/AirplaneResponse');
-const { dbError } = require('../constants/errors');
+const CustomError = require('../classes/CustomError');
 
 const getAll = async params => {
   try {
     const airplanes = await airplaneService.find(params);
 
-    return new AirplaneResponse(false, airplanes);
+    return airplanes;
   } catch (err) {
-    return new AirplaneResponse(true, dbError.get);
+    return new CustomError(false, err.message);
   }
 };
 
@@ -16,19 +15,17 @@ const getById = async ({ airplaneId }) => {
   try {
     const airplane = await airplaneService.findById(airplaneId);
 
-    return new AirplaneResponse(false, airplane);
+    return airplane;
   } catch (err) {
-    return new AirplaneResponse(true, dbError.get);
+    return new CustomError(false, err.message);
   }
 };
 
 const add = async airplane => {
   try {
-    await airplaneService.add(airplane);
-
-    return new AirplaneResponse();
+    return await airplaneService.add(airplane);
   } catch (err) {
-    return new AirplaneResponse(true, dbError.create);
+    return new CustomError(false, err.message);
   }
 };
 
