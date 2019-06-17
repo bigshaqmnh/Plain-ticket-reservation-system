7f26c9476729b55b9228a817feebe82c4897c261
mike_kotikov <kmikecoder@gmail.com>
2019-06-17 11:13:47 +0300

    fix typos

--- server/constants/errors.js
+++ server/constants/errors.js
@@ -2,12 +2,12 @@ const statusCode = require('http-status-codes');
 
 const reqError = {
   logIn: { status: statusCode.CONFLICT, message: 'User not found. Check your credentials.' },
-  singUp: { status: statusCode.CONFLICT, message: 'User with such email already exists.' }
+  signUp: { status: statusCode.CONFLICT, message: 'User with such email already exists.' }
 };
 
 const dbError = {
   logIn: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to log in.' },
-  singUp: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to sign up.' },
+  signUp: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to sign up.' },
   get: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to get data.' },
   create: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to create new instance.' },
   update: { status: statusCode.SERVICE_UNAVAILABLE, message: 'Unable to update the instance.' }
