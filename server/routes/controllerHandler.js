968955574bc947edfbc20f5cc135ebdb7a510390
mike_kotikov <kmikecoder@gmail.com>
2019-06-17 17:25:51 +0300

    change response send logic

--- server/routes/controllerHandler.js
+++ server/routes/controllerHandler.js
@@ -1,26 +1,16 @@
-const status = require('../constants/status');
-const ControllerError = require('../classes/ControllerError');
-const ServiceError = require('../classes/ServiceError');
+const statusCode = require('http-status-codes');
 
 const controllerHandler = (controller, params) => async (req, res, next) => {
   const boundParams = params(req, res, next);
-  const method = req.method;
 
   try {
     const data = await controller(boundParams);
 
-    return data ? res.status(status.success[method]).json(data) : res.sendStatus(status.success[method]);
+    return data ? res.status(statusCode.OK).json(data) : res.sendStatus(statusCode.OK);
   } catch (err) {
     console.error(err);
-    if (err instanceof ControllerError) {
-      return res.sendStatus(status.fail[method]);
-    }
 
-    if (err instanceof ServiceError) {
-      return res.sendStatus(status.fail.DB);
-    }
-
-    return res.sendStatus(status.fail.SERVER);
+    return res.sendStatus(err.status || statusCode.INTERNAL_SERVER_ERROR);
   }
 };
 
