7d6d3d558da4cc5bffd89a99326a9042b2a9b331
mike_kotikov <kmikecoder@gmail.com>
2019-06-17 13:00:09 +0300

    fix undefined passwordHash return

--- server/services/user.js
+++ server/services/user.js
@@ -4,8 +4,7 @@ const jwt = require('jsonwebtoken');
 const findByEmail = async email => {
   try {
     const user = await db.user.findOne({
-      where: { email },
-      attributes: ['id', 'username', 'email']
+      where: { email }
     });
     return user && user.dataValues;
   } catch (err) {
@@ -36,6 +35,6 @@ const comparePasswords = async (reqPassword, dbPassword) => await bcrypt.compare
 
 const hashPassword = async password => await bcrypt.hash(password, 10);
 
-const generateToken = async payload => await jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: 36000 });
+const generateToken = async payload => await jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1d' });
 
 module.exports = { findByEmail, checkIfExists, add, comparePasswords, hashPassword, generateToken };
