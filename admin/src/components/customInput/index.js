c3275604b043cd1f38bcda257206186d423164e1
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 11:47:23 +0300

    add string formatter for label and placeholder

--- admin/src/components/customInput/index.js
+++ admin/src/components/customInput/index.js
@@ -2,13 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
 
+import stringFormatter from '../../helpers/stringFormatter';
+
 function CustomInput(props) {
-  const { label, type, name, value, placeholder, invalidFeedback, isValid, onChange } = props;
+  const { label, type, name, value, placeholder, invalidFeedback, isValid, onChange, isDisabled } = props;
 
   return (
     <Form.Group className="input">
-      <Form.Label>{label}</Form.Label>
-      <Form.Control type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} />
+      <Form.Label>{stringFormatter.toRegular(label)}</Form.Label>
+      {isDisabled ? (
+        <Form.Control
+          type={type}
+          name={name}
+          value={value}
+          placeholder={stringFormatter.toRegular(placeholder)}
+          onChange={onChange}
+          disabled
+        />
+      ) : (
+        <Form.Control
+          type={type}
+          name={name}
+          value={value}
+          placeholder={stringFormatter.toRegular(placeholder)}
+          onChange={onChange}
+        />
+      )}
       {!isValid && <Form.Text style={{ color: 'red' }}>{invalidFeedback}</Form.Text>}
     </Form.Group>
   );
@@ -18,18 +37,22 @@ CustomInput.propTypes = {
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
   name: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  placeholder: PropTypes.string,
   invalidFeedback: PropTypes.string,
   isValid: PropTypes.bool,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func,
+  isDisabled: PropTypes.bool
 };
 
 CustomInput.defaultProps = {
   type: 'text',
   value: '',
+  placeholder: 'Input value',
   invalidFeedback: 'Invalid input.',
-  isValid: true
+  isValid: true,
+  onChange: null,
+  isDisabled: false
 };
 
 export default CustomInput;
