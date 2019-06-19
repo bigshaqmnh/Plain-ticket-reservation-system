1e18c690385f75490938c05bb49b39cf2e41e105
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 11:32:52 +0300

    rename to token.js

--- admin/src/helpers/index.js
+++
@@ -1,9 +0,0 @@
-const tokenKey = 'token';
-
-export const saveUserToken = token => {
-  localStorage.setItem(tokenKey, token);
-};
-
-export const deleteUserToken = () => {
-  localStorage.removeItem(tokenKey);
-};
