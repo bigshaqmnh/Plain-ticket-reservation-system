bfcde42823e0f99099ea42c19c8bda3a2e1cb6e4
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 12:55:32 +0300

    add route for update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,4 +8,10 @@ router.post('/login', controllerHandler(authController.logIn, (req, res, next) =
 
 router.post('/signUp', controllerHandler(authController.signUp, (req, res, next) => req.body));
 
+router.put(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  controllerHandler(authController.update, (req, res, next) => ({ id: req.user.id, user: req.body }))
+);
+
 module.exports = router;
