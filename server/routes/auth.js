b587ae8e161e63316ca6bbc5456c888d413055c2
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 18:07:11 +0300

    fix passport import

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 const controllerHandler = require('./controllerHandler');
 const authController = require('../controllers/auth');
