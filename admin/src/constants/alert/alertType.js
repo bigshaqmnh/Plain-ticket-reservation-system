9ddd43311cd9b9364c3ed181b5577a604cf09fef
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 11:38:26 +0300

    rename to alertType.js

--- admin/src/constants/alert/alertType.js
+++ admin/src/constants/alert/alertType.js
@@ -1,12 +1,16 @@
-const alertText = {
+import componentStyles from '../componentStyles';
+
+const alertType = {
   emptyInput: {
+    variant: componentStyles.error,
     heading: 'Empty input!',
     mainText: 'Please, fill in all the fields.'
   },
   invalidInput: {
+    variant: componentStyles.error,
     heading: 'Invalid input!',
     mainText: 'Please, verify your input according to the hints.'
   }
 };
 
-export default alertText;
+export default alertType;
