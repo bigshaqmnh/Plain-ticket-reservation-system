3893ea5ed9a4cb9b97143e46b24a71e0ea1b12c1
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 11:39:27 +0300

    add message for required and not a number

--- admin/src/constants/validationText.js
+++ admin/src/constants/validationText.js
@@ -1,6 +1,8 @@
 const validationText = {
   requiredEmail: 'Email is required.',
   requiredPassword: 'Password is required.',
+  required: 'This field is required.',
+  notNumber: 'The value must be a number.',
   invalidEmail: 'Please, check your email for being correct.',
   invalidPassword: 'Password should have at least one digit. It should not contain symbols.',
   short: 'Too short!',
