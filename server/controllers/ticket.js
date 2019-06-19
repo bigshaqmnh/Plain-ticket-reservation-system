d15d4b14408b934b754ab4a237c3679f012b14ae
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 18:06:48 +0300

    refactor getByUserId function

--- server/controllers/ticket.js
+++ server/controllers/ticket.js
@@ -8,20 +8,27 @@ const getByUserId = async ({ userId, page: pageNum, limit }) => {
   try {
     const tickets = await ticketService.findByUserId(userId, pageNum, limit);
 
-    const ticketsInfo = await Promise.all(
-      tickets.data.map(async ticket => {
-        const seat = await seatService.findById(ticket.seatId);
-        const cost = await costService.findById(ticket.costId);
-        const flightInfo = await flightService.findById(cost.cost.flightId);
+    const seatIds = tickets.data.map(ticket => ticket.seatId);
+    const costIds = tickets.data.map(ticket => ticket.costId);
 
-        return {
-          ...ticket,
-          ...seat,
-          ...cost,
-          ...flightInfo
-        };
-      })
-    );
+    const seats = await seatService.findByIds(seatIds);
+    const costs = await costService.findByIds(costIds);
+
+    const flightIds = costs.map(cost => cost.flightId);
+
+    const flights = await flightService.findByIds(flightIds);
+
+    const ticketsInfo = tickets.data.map(ticket => {
+      const seat = seats.find(seat => seat.id === ticket.seatId);
+      const cost = costs.find(cost => cost.id === ticket.costId);
+      const flight = flights.find(flight => flight.id === cost.flightId);
+
+      return {
+        ...flight,
+        ...cost,
+        seat
+      };
+    });
 
     return { data: ticketsInfo, nextPage: tickets.nextPage };
   } catch (err) {
