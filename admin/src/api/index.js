c80fdf75236f684514fa20f7561446e02dc15bc4
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 11:55:51 +0300

    add airplane api, refactor to try / catch

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -1,21 +1,56 @@
 import * as axios from 'axios';
 
 import * as config from '../config/config.json';
-import { saveUserToken, deleteUserToken } from '../helpers';
+import { getUserToken, saveUserToken, deleteUserToken } from '../helpers/token';
 
-export default {
-  logIn: userData => {
-    axios({
-      method: 'POST',
-      url: `${config.baseUrl}/auth/login`,
-      data: {
-        email: userData.email,
-        password: userData.password
-      }
-    })
-      .then(response => saveUserToken(response.data))
-      .catch(error => console.error('Error: unable to log in.', error));
+export const authApi = {
+  logIn: async userData => {
+    try {
+      const token = await axios.post(`${config.baseUrl}/auth/login`, userData);
+
+      saveUserToken(token.data);
+    } catch (err) {
+      console.error('Error: unable to log in.', err);
+    }
   },
 
   logOut: () => deleteUserToken()
+};
+
+export const airplaneApi = {
+  getAirplanes: async params => {
+    try {
+      const airplanes = await axios.get(`${config.baseUrl}/airplanes`, { params });
+
+      return airplanes.data;
+    } catch (err) {
+      console.error('Error: unable to get airplanes.', err);
+    }
+  },
+
+  addAirplane: async airplane => {
+    try {
+      return await axios({
+        method: 'POST',
+        Authorization: getUserToken(),
+        url: `${config.baseUrl}/airplanes`,
+        data: airplane
+      });
+    } catch (err) {
+      console.error('Error: unable to add new airplane.', err);
+    }
+  },
+
+  updateAirplane: async airplane => {
+    try {
+      return await axios({
+        method: 'PUT',
+        Authorization: getUserToken(),
+        url: `${config.baseUrl}/airplanes/${airplane.id}`,
+        data: airplane
+      });
+    } catch (err) {
+      console.error('Error: unable to update the airplane.', err);
+    }
+  }
 };
