cf4124c1748811e2d98200320b644f085260191a
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 13:02:11 +0300

    remove service unavailable status

--- server/constants/responseStatus.js
+++ server/constants/responseStatus.js
@@ -1,9 +1,11 @@
 const statusCode = require('http-status-codes');
 
 module.exports = {
+  ok: statusCode.OK,
   conflict: statusCode.CONFLICT,
+  unauthorized: statusCode.UNAUTHORIZED,
   forbidden: statusCode.FORBIDDEN,
   badReqest: statusCode.BAD_REQUEST,
-  unavailable: statusCode.SERVICE_UNAVAILABLE,
-  notFound: statusCode.NOT_FOUND
+  notFound: statusCode.NOT_FOUND,
+  fatal: statusCode.INTERNAL_SERVER_ERROR
 };
