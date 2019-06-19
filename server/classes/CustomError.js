3bd2bb808e2dd42956b2aeab0778a8cbad63871d
mike_kotikov <kmikecoder@gmail.com>
2019-06-19 12:38:26 +0300

    change default response status, remove name prop

--- server/classes/CustomError.js
+++ server/classes/CustomError.js
@@ -3,8 +3,7 @@ const responseStatus = require('../constants/responseStatus');
 class CustomError extends Error {
   constructor({ status, message }) {
     super(message);
-    this.name = 'CustomError';
-    this.status = status || responseStatus.badReqest;
+    this.status = status || responseStatus.fatal;
   }
 }
 
