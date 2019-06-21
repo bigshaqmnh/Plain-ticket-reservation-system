4e79b58b65c1351c4daf0038be3fd7e4e9466cc2
mike_kotikov <kmikecoder@gmail.com>
2019-06-21 12:23:27 +0300

    refactor router import, remove user update route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,4 @@
-var express = require('express');
-var router = express.Router();
-const passport = require('passport');
+const router = require('express').Router();
 
 const controllerHandler = require('./controllerHandler');
 const authController = require('../controllers/auth');
@@ -8,11 +6,5 @@ const authController = require('../controllers/auth');
 router.post('/login', controllerHandler(authController.logIn, (req, res, next) => req.body));
 
 router.post('/signUp', controllerHandler(authController.signUp, (req, res, next) => req.body));
-
-router.put(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  controllerHandler(authController.update, (req, res, next) => ({ id: req.user.id, user: req.body }))
-);
 
 module.exports = router;
