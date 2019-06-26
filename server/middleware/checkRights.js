7d0484a12629aaef63160c0555a8aa608713e72d
mike_kotikov <kmikecoder@gmail.com>
2019-06-26 15:09:42 +0300

    rename

--- server/middleware/checkRights.js
+++
@@ -1,5 +0,0 @@
-const statusCode = require('http-status-codes');
-
-const hasRights = (req, res, next) => (req.user.isAdmin ? next() : res.sendStatus(statusCode.FORBIDDEN));
-
-module.exports = hasRights;
