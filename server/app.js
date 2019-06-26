7dd2deacc299ca6b407b0d3ebb35547cc9fcaf08
mike_kotikov <kmikecoder@gmail.com>
2019-06-26 15:00:43 +0300

    add error handler, configure admin routes

--- server/app.js
+++ server/app.js
@@ -9,14 +9,19 @@ require('./passportSetup')(passport);
 require('./sequelizeSetup');
 
 const authRouter = require('./routes/auth');
-const airplaneRouter = require('./routes/airplane');
-const airportRouter = require('./routes/airport');
-const flightRouter = require('./routes/flight');
+const adminAirplaneRouter = require('./routes/admin/airplane');
+const adminAirportRouter = require('./routes/admin/airport');
+const adminCostRouter = require('./routes/admin/cost');
 const costRouter = require('./routes/cost');
+const adminFlightRouter = require('./routes/admin/flight');
+const flightRouter = require('./routes/flight');
 const luggageOptionRouter = require('./routes/luggageOption');
+const adminSeatRouter = require('./routes/admin/seat');
 const seatRouter = require('./routes/seat');
 const ticketRouter = require('./routes/ticket');
 const userRouter = require('./routes/user');
+
+const checkAdminRights = require('./middleware/checkAdminRights');
 
 const app = express();
 
@@ -34,18 +39,21 @@ app.use(
 );
 
 app.use('/auth', authRouter);
-app.use('/airplanes', airplaneRouter);
-app.use('/airports', airportRouter);
-app.use('/flights', flightRouter);
+app.use('/admin/airplanes', passport.authenticate('jwt', { session: false }), checkAdminRights, adminAirplaneRouter);
+app.use('/admin/airports', passport.authenticate('jwt', { session: false }), checkAdminRights, adminAirportRouter);
+app.use('/admin/costs', passport.authenticate('jwt', { session: false }), checkAdminRights, adminCostRouter);
 app.use('/costs', costRouter);
+app.use('/admin/flights', passport.authenticate('jwt', { session: false }), checkAdminRights, adminFlightRouter);
+app.use('/flights', flightRouter);
 app.use('/luggageOptions', luggageOptionRouter);
+app.use('/admin/seats', passport.authenticate('jwt', { session: false }), checkAdminRights, adminSeatRouter);
 app.use('/seats', seatRouter);
-app.use('/tickets', ticketRouter);
-app.use('/users', userRouter);
+app.use('/tickets', passport.authenticate('jwt', { session: false }), ticketRouter);
+app.use('/users', passport.authenticate('jwt', { session: false }), userRouter);
 
-app.use((req, res) => {
-  res.sendStatus(statusCode.NOT_FOUND);
-});
+app.use((req, res, next) => res.sendStatus(statusCode.NOT_FOUND));
+
+app.use((err, req, res, next) => res.sendStatus(statusCode.INTERNAL_SERVER_ERROR));
 
 const port = process.env.SERVER_PORT || '3000';
 
