c88036dd558a27ee5e2196067aecca07a0ee4706
mike_kotikov <kmikecoder@gmail.com>
2019-07-04 14:39:59 +0300

    implement pagination with react-js-pagination

--- admin/src/hooks/useFetchData.js
+++ admin/src/hooks/useFetchData.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react';
 
-import { resultsPerPageLimit } from '../constants/common';
-
 function useFetchData(apiMethod) {
   const [items, setItems] = useState([]);
+  const [itemsCount, setItemsCount] = useState(0);
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [maxPage, setMaxPage] = useState(1);
+
   const [isLoading, setIsLoading] = useState(true);
 
   const [searchText, setSearchText] = useState('');
@@ -16,7 +15,7 @@ function useFetchData(apiMethod) {
       const { data, count } = await apiMethod(params);
 
       setItems(data);
-      setMaxPage(Math.ceil(count / resultsPerPageLimit));
+      setItemsCount(count);
       setIsLoading(false);
     } catch (err) {
       console.error(err);
@@ -29,7 +28,7 @@ function useFetchData(apiMethod) {
     fetchData({ page: currentPage, query: searchText });
   }, [searchText, currentPage]);
 
-  return { items, setItems, isLoading, searchText, setSearchText, currentPage, setCurrentPage, maxPage };
+  return { items, setItems, itemsCount, isLoading, searchText, setSearchText, currentPage, setCurrentPage };
 }
 
 export default useFetchData;
