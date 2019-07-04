3260f08e7f504033bcf32f8b0fec43dd060ae00d
mike_kotikov <kmikecoder@gmail.com>
2019-07-04 15:47:48 +0300

    create FlightEdit container

---
+++ admin/src/containers/flights/FlightEdit/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CustomInput from '../../../components/customInput';
+import CustomButton from '../../../components/customButton';
+import componentStyles from '../../../constants/componentStyles';
+import stringFormatter from '../../../helpers/stringFormatter';
+
+function FlightEdit({ flight, handleBack, handleSave }) {
+  return (
+    <>
+      {Object.keys(flight).map(key => {
+        if (key === 'id') {
+          return;
+        }
+
+        const label = stringFormatter.toRegular(key);
+        const value = flight[key] instanceof Date ? flight[key].toDateString() : flight[key];
+
+        return <CustomInput key={key} label={label} name={key} value={value} />;
+      })}
+      <CustomButton variant={componentStyles.default} text="Back" onClick={handleBack} />
+      <CustomButton variant={componentStyles.success} text="Save" onClick={handleSave} />
+    </>
+  );
+}
+
+FlightEdit.propTypes = {
+  flight: PropTypes.shape({
+    id: PropTypes.number,
+    departureTime: PropTypes.instanceOf(Date),
+    arrivalTime: PropTypes.instanceOf(Date),
+    luggageOverweightCost: PropTypes.number,
+    isCancelled: PropTypes.string,
+    departureAirport: PropTypes.string,
+    arrivalAirport: PropTypes.string
+  }).isRequired,
+  handleBack: PropTypes.func.isRequired,
+  handleSave: PropTypes.func.isRequired
+};
+
+export default FlightEdit;
