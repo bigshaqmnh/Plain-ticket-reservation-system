0be26607a77a6c0c076f5728eeae6c5ec918e0f4
mike_kotikov <kmikecoder@gmail.com>
2019-07-05 18:11:43 +0300

    add departure and arrival airports props

--- admin/src/helpers/formatFlights.js
+++ admin/src/helpers/formatFlights.js
@@ -1,11 +1,10 @@
-const toLocaleDate = date => new Date(date).toLocaleString();
-
 const formatFlights = flights =>
   flights.map(flight => ({
     ...flight,
-    departureTime: toLocaleDate(flight.departureTime),
-    arrivalTime: toLocaleDate(flight.arrivalTime),
-    isCancelled: flight.isCancelled ? '\u2714' : '\u274c'
+    departureTime: new Date(flight.departureTime),
+    arrivalTime: new Date(flight.arrivalTime),
+    departureAirport: flight.departureAirport.name,
+    arrivalAirport: flight.arrivalAirport.name
   }));
 
 export default formatFlights;
