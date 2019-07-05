920bb44f425e165e5c344258ef4a6825fd69b902
mike_kotikov <kmikecoder@gmail.com>
2019-07-05 18:08:40 +0300

    refactor pagination, screen switch

--- admin/src/containers/flights/index.js
+++ admin/src/containers/flights/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Spinner } from 'react-bootstrap';
+import Pagination from 'react-js-pagination';
 
 import FlightDetails from './FlightDetails';
 import FlightAdd from './FlightAdd';
+import FlightEdit from './FlightEdit';
 import CustomInput from '../../components/customInput';
 import CustomTable from '../../components/customTable';
-import CustomPagination from '../../components/customPagination';
 import CustomButton from '../../components/customButton';
 import CustomAlert from '../../components/customAlert';
 
@@ -16,78 +17,75 @@ import flightApi from '../../api/flight';
 
 import componentStyles from '../../constants/componentStyles';
 import { resultsPerPageLimit } from '../../constants/common';
+import screen from '../../constants/screens';
 
 import formatFlights from '../../helpers/formatFlights';
+import getHandlers from '../../helpers/getHandlers';
 
 function FlightsContainer() {
   const {
     items,
-    setItems: setFlights,
+    setItems,
+    itemsCount,
     isLoading,
     searchText,
     setSearchText,
     currentPage,
-    setCurrentPage,
-    maxPage
+    setCurrentPage
   } = useFetchData(flightApi.getFlights);
 
   const flights = formatFlights(items);
 
   const { alert, setAlert, showAlert, setShowAlert } = useAlert();
 
-  const [selectedFlight, setSelectedFlight] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
 
-  const [showInfoScreen, setShowInfoScreen] = useState(false);
-  const [showAddScreen, setShowAddScreen] = useState(false);
-
-  const handleClick = event => {
-    const { id } = event.currentTarget;
-    const selected = flights.find(flight => flight.id === +id);
-
-    setSelectedFlight(selected);
-    setShowInfoScreen(true);
+  const screens = {
+    [screen.TABLE]: renderTableScreen,
+    [screen.DETAILS]: renderDetailsScreen,
+    [screen.ADD]: renderAddScreen,
+    [screen.EDIT]: renderEditScreen
   };
 
-  const handleBack = () => {
-    showInfoScreen && setShowInfoScreen(false);
-    showAddScreen && setShowAddScreen(false);
-  };
+  const [currentScreen, setCurrentScreen] = useState(screen.TABLE);
 
-  const handleSearch = ({ target }) => {
-    setSearchText(target.value);
-  };
+  const {
+    handleClickItem,
+    handleSearchItem,
+    handleShowAddScreen,
+    handleShowEditScreen,
+    handleBackAction
+  } = getHandlers({
+    items: flights,
+    setSelectedItem,
+    setCurrentScreen,
+    setSearchText
+  });
 
-  const handlePagination = ({ target }) => {
-    const selectedPage = +target.name || +target.parentNode.name;
-
-    if (selectedPage) {
-      setCurrentPage(selectedPage);
-    }
-  };
-
-  const handleSave = async data => {
+  const handleAddItem = async data => {
     try {
-      handleBack();
+      handleBackAction();
 
       const newFlight = await flightApi.addFlight(data);
-
-      setCurrentPage(flights.length >= resultsPerPageLimit ? maxPage + 1 : maxPage);
+      const maxPage = Math.ceil(itemsCount / resultsPerPageLimit);
 
       if (currentPage === maxPage) {
-        setFlights([...flights, newFlight]);
+        items.length >= resultsPerPageLimit ? setCurrentPage(maxPage + 1) : setItems([...items, newFlight]);
+      } else {
+        setCurrentPage(maxPage);
       }
 
       setAlert({
         variant: componentStyles.success,
-        heading: 'Saved',
-        mainText: 'Airplane was successfully saved.',
+        heading: 'Added',
+        mainText: 'Flight was successfully added.',
         isShown: setShowAlert
       });
     } catch (err) {
       setAlert({
         variant: componentStyles.error,
-        heading: 'Not Saved',
-        mainText: 'An error occured while saving airplane data.',
+        heading: 'Not Added',
+        mainText: 'An error occured while adding new flight.',
         isShown: setShowAlert
       });
     } finally {
@@ -95,18 +93,44 @@ function FlightsContainer() {
     }
   };
 
-  const handleAdd = () => setShowAddScreen(true);
+  const handleEditItem = async data => {
+    try {
+      handleBackAction();
+
+      const updatedFlight = await flightApi.updateFlight(data);
+
+      setItems(items.map(flight => (flight.id === updatedFlight.id ? updatedFlight : flight)));
+
+      setAlert({
+        variant: componentStyles.success,
+        heading: 'Updated',
+        mainText: 'Flight was successfully updated.',
+        isShown: setShowAlert
+      });
+    } catch (err) {
+      setAlert({
+        variant: componentStyles.error,
+        heading: 'Not updated',
+        mainText: 'An error occured while updating flight data.',
+        isShown: setShowAlert
+      });
+    } finally {
+      setShowAlert(true);
+    }
+  };
 
   const renderTable = () =>
-    flights.length ? (
+    flights && flights.length ? (
       <>
-        <CustomTable headers={Object.keys(flights[0])} items={flights} onClick={handleClick} />
-        {maxPage > 1 && (
-          <CustomPagination
-            currentPage={currentPage}
-            lastPage={maxPage}
-            isLarge={maxPage >= resultsPerPageLimit}
-            handlePagination={handlePagination}
+        <CustomTable headers={Object.keys(flights[0])} items={flights} onClick={handleClickItem} />
+        {itemsCount > resultsPerPageLimit && (
+          <Pagination
+            itemClass="page-item"
+            linkClass="page-link"
+            activePage={currentPage}
+            totalItemsCount={itemsCount}
+            onChange={setCurrentPage}
+            hideDisabled
           />
         )}
       </>
@@ -114,34 +138,36 @@ function FlightsContainer() {
       <h1>No Data.</h1>
     );
 
-  const renderPage = () => (
-    <>
-      <CustomInput
-        label="Search"
-        name="flight-search"
-        value={searchText}
-        placeholder="Search flights"
-        onChange={handleSearch}
-      />
-      <CustomButton variant={componentStyles.success} text="Add flight" onClick={handleAdd} />
-      {isLoading ? <Spinner animation="border" variant={componentStyles.default} /> : renderTable()}
-      {showAlert && <CustomAlert props={alert} />}
-    </>
-  );
-
-  const renderScreen = () =>
-    showInfoScreen ? (
-      <FlightDetails
-        name={selectedFlight.name}
-        type={selectedFlight.type}
-        maxLuggageCarryWeight={selectedFlight.maxLuggageCarryWeight}
-        handleBack={handleBack}
-      />
-    ) : (
-      <FlightAdd handleSave={handleSave} handleBack={handleBack} />
+  function renderTableScreen() {
+    return (
+      <>
+        <CustomInput
+          label="Search"
+          name="flight-search"
+          value={searchText}
+          placeholder="Search flights"
+          onChange={handleSearchItem}
+        />
+        <CustomButton variant={componentStyles.success} text="Add flight" onClick={handleShowAddScreen} />
+        {isLoading ? <Spinner animation="border" variant={componentStyles.default} /> : renderTable()}
+        {showAlert && <CustomAlert props={alert} />}
+      </>
     );
+  }
 
-  return showInfoScreen || showAddScreen ? renderScreen() : renderPage();
+  function renderDetailsScreen() {
+    return <FlightDetails flight={selectedItem} handleBack={handleBackAction} handleEdit={handleShowEditScreen} />;
+  }
+
+  function renderAddScreen() {
+    return <FlightAdd handleBack={handleBackAction} handleSave={handleAddItem} />;
+  }
+
+  function renderEditScreen() {
+    return <FlightEdit flight={selectedItem} handleBack={handleBackAction} handleSave={handleEditItem} />;
+  }
+
+  return screens[currentScreen]();
 }
 
 export default FlightsContainer;
