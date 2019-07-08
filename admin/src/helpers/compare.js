c2c820a2efb34c3cd42e2ed42e80118e379bae90
mike_kotikov <kmikecoder@gmail.com>
2019-07-08 17:58:47 +0300

    add function to compare dates

--- admin/src/helpers/compare.js
+++ admin/src/helpers/compare.js
@@ -1,3 +1,8 @@
 const objects = (obj1, obj2) => JSON.stringify(obj1) === JSON.stringify(obj2);
 
-export default { objects };
+const dates = (date1, date2) =>
+  date1.getFullYear() === date2.getFullYear() &&
+  date1.getMonth() === date2.getMonth() &&
+  date1.getDate() === date2.getDate();
+
+export default { objects, dates };
