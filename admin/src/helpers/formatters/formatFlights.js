6764d8c1111172a2ee19e8b51974f8623ea4b0c9
mike_kotikov <kmikecoder@gmail.com>
2019-07-08 17:59:35 +0300

    rename and move to separate folder

--- admin/src/helpers/formatters/formatFlights.js
+++ admin/src/helpers/formatters/formatFlights.js
@@ -1,10 +1,15 @@
-const formatFlights = flights =>
-  flights.map(flight => ({
+const formatFlights = flights => {
+  if (!flights) {
+    return;
+  }
+
+  return flights.map(flight => ({
     ...flight,
     departureTime: new Date(flight.departureTime),
     arrivalTime: new Date(flight.arrivalTime),
     departureAirport: flight.departureAirport.name,
     arrivalAirport: flight.arrivalAirport.name
   }));
+};
 
 export default formatFlights;
--- admin/src/helpers/stringFormatter.js
+++
@@ -1,3 +0,0 @@
-const toRegular = str => str.replace(/([A-Z])/g, ' $1').replace(/^./, letter => letter.toUpperCase());
-
-export default { toRegular };
