b289737772c5743227291f76025463a69eec7941
mike_kotikov <kmikecoder@gmail.com>
2019-07-09 18:31:44 +0300

    add search by field functionality

--- server/services/airplane.js
+++ server/services/airplane.js
@@ -1,26 +1,33 @@
-const find = async ({ page, query: inputString, limit: resLimit } = {}) => {
+const find = async ({ page, query: inputString, field, limit: resLimit } = {}) => {
   const limit = resLimit || 10;
   const pageNum = +page || 1;
   const offset = pageNum * limit - limit;
-  const searchParam = inputString
-    ? {
-        [db.op.or]: [
-          {
-            name: { [db.op.iLike]: `%${inputString}%` }
-          },
-          {
-            type: { [db.op.iLike]: `%${inputString}%` }
-          }
-        ]
-      }
-    : {};
+  let searchParam = {};
+  const attributes = field ? ['id', field] : ['id', 'name', 'type', 'maxLuggageCarryWeight'];
+
+  if (field && inputString) {
+    searchParam = {
+      [field]: { [db.op.iLike]: `%${inputString}%` }
+    };
+  } else if (inputString) {
+    searchParam = {
+      [db.op.or]: [
+        {
+          name: { [db.op.iLike]: `%${inputString}%` }
+        },
+        {
+          type: { [db.op.iLike]: `%${inputString}%` }
+        }
+      ]
+    };
+  }
 
   const airplanes = await db.airplane.findAndCountAll({
     where: searchParam,
     offset,
     limit,
     order: [['id', 'ASC']],
-    attributes: ['id', 'name', 'type', 'maxLuggageCarryWeight']
+    attributes
   });
 
   const { rows, count } = airplanes;
