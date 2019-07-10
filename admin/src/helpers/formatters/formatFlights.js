ebcf6b0fb5a28ebd21bc43c5933ed05cfda1267a
mike_kotikov <kmikecoder@gmail.com>
2019-07-10 13:58:26 +0300

    refactor formatFlights

--- admin/src/helpers/formatters/formatFlights.js
+++ admin/src/helpers/formatters/formatFlights.js
@@ -6,10 +6,7 @@ const formatFlights = flights => {
   return flights.map(flight => ({
     ...flight,
     departureTime: new Date(flight.departureTime),
-    arrivalTime: new Date(flight.arrivalTime),
-    departureAirport: flight.departureAirport.name,
-    arrivalAirport: flight.arrivalAirport.name,
-    airplane: flight.airplane.name
+    arrivalTime: new Date(flight.arrivalTime)
   }));
 };
 
