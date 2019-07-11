08b0d1fd6eb9a80b02742be5d77f23eb5322f093
Mike Kotikov <kmikecoder@gmail.com>
2019-07-11 23:38:26 +0300

    create route to get user info

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const wrapHandlerToCatchError = require('../middleware/handlerWrapper');
 const userHandler = require('../handlers/user');
 
+router.get('/info', wrapHandlerToCatchError(userHandler.getInfo));
+
 router.put('/', wrapHandlerToCatchError(userHandler.update));
 
 module.exports = router;
