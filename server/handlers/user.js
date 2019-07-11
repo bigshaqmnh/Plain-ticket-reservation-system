dad0a44077b47b9fa2d680ab589dba444a603a76
Mike Kotikov <kmikecoder@gmail.com>
2019-07-11 23:38:10 +0300

    create function to get user info

--- server/handlers/user.js
+++ server/handlers/user.js
@@ -2,10 +2,20 @@ const statusCode = require('http-status-codes');
 
 const userController = require('../controllers/user');
 
+const getInfo = (req, res, next) => {
+  if (!req.user) {
+    res.sendStatus(statusCode.NOT_FOUND);
+  }
+
+  const { username, email } = req.user;
+
+  res.status(statusCode.OK).json({ username, email });
+};
+
 const update = async (req, res, next) => {
   await userController.update(req.user.id, req.body);
 
   res.sendStatus(statusCode.OK);
 };
 
-module.exports = { update };
+module.exports = { update, getInfo };
