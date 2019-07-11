cdd72a074a38e448d03f2547cf952a112c36efcd
Mike Kotikov <kmikecoder@gmail.com>
2019-07-11 23:30:40 +0300

    refactor logIn, get rid of logOut functions

--- admin/src/api/auth.js
+++ admin/src/api/auth.js
@@ -1,13 +1,6 @@
 import fetchData from './fetchData';
 import * as config from '../config/config.json';
-import { saveUserToken, deleteUserToken } from '../helpers/token';
 
 export default {
-  logIn: async userData => {
-    const token = await fetchData({ method: 'POST', url: `${config.baseUrl}/auth/login`, data: userData });
-
-    saveUserToken(token);
-  },
-
-  logOut: () => deleteUserToken()
+  logIn: async userData => await fetchData({ method: 'POST', url: `${config.baseUrl}/auth/login`, data: userData })
 };
