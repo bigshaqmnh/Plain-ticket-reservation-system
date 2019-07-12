8646098bf436ef6c9958fb03e3597124a377e96e
mike_kotikov <kmikecoder@gmail.com>
2019-07-12 18:12:37 +0300

    add unautharized error handler

--- admin/src/api/fetchData.js
+++ admin/src/api/fetchData.js
@@ -1,17 +1,20 @@
-import * as axios from 'axios';
+import axios from 'axios';
 
 import { getUserToken } from '../helpers/token';
 
 const fetchData = async ({ needAuth, ...requestParams }) => {
   try {
-    const response = await axios(
+    const { data } = await axios(
       needAuth
         ? { ...requestParams, headers: { ...requestParams.headers, Authorization: `Bearer ${getUserToken()}` } }
         : requestParams
     );
 
-    return response.data;
+    return data;
   } catch (err) {
+    if (err.response.status === 401) {
+      return { data: null };
+    }
     console.error('Error: unable to fetch data.', err);
     throw err;
   }
