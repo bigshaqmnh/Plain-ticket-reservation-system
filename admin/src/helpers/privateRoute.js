6b225471687817eaa43b6c7a35b3f92effe588bf
mike_kotikov <kmikecoder@gmail.com>
2019-07-15 18:29:00 +0300

    add header component to all private routes

--- admin/src/helpers/privateRoute.js
+++ admin/src/helpers/privateRoute.js
@@ -2,12 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import HeaderContainer from '../containers/header';
+
 import { getUserToken } from './token';
 
 const PrivateRoute = ({ component: Component, ...args }) => (
-  <Route {...args} render={props => (getUserToken() ? <Component {...props} /> : <Redirect to="/auth" />)} />
+  <Route
+    {...args}
+    render={props =>
+      getUserToken() ? (
+        <>
+          <HeaderContainer /> <Component {...props} />
+        </>
+      ) : (
+        <Redirect to="/auth" />
+      )
+    }
+  />
 );
-
 PrivateRoute.propTypes = {
   component: PropTypes.func
 };
