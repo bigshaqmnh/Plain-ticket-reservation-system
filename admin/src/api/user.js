8f935fe721bb1882e3aaf3d78cdaf202502c726d
mike_kotikov <kmikecoder@gmail.com>
2019-07-18 18:40:57 +0300

    create function to update user data

--- admin/src/api/user.js
+++ admin/src/api/user.js
@@ -2,5 +2,6 @@ import fetchData from './fetchData';
 import * as config from '../config/config.json';
 
 export default {
-  getUserInfo: () => fetchData({ method: 'GET', needAuth: true, url: `${config.baseUrl}/users/info` })
+  getUserInfo: () => fetchData({ method: 'GET', needAuth: true, url: `${config.baseUrl}/users/info` }),
+  updateUser: user => fetchData({ method: 'PUT', needAuth: true, url: `${config.baseUrl}/users`, data: user })
 };
