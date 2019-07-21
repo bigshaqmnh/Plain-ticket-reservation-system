61f8d6e0df982f1e7f8647470281193333514ebb
Mike Kotikov <kmikecoder@gmail.com>
2019-07-22 00:29:46 +0300

    refactor get and update user controller functions

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,18 +3,42 @@ const fs = require('fs');
 
 const userService = require('../services/user');
 
-const getUserInfo = user => ({ username: user.username, email: user.email });
+const photosStoragePath = '../server/static/photos';
+const parseBase64 = {
+  data: 'data:image/',
+  base: ';base64'
+};
 
-const getUserPhoto = userId => {
-  const filePath = path.resolve('../server/static/photos', `${userId}.jpg`);
-  const photo = fs.readFileSync(filePath);
+const _clearFolder = folderName => {
+  const files = fs.readdirSync(folderName);
+  files.forEach(file => fs.unlinkSync(path.join(folderName, file)));
+};
+
+const getUserInfo = async userId => {
+  const user = await userService.findById(userId);
+
+  if (!user) {
+    return;
+  }
+
+  const photo = fs.readFileSync(user.photo);
   const encodedPhoto = Buffer.from(photo).toString('base64');
 
-  return encodedPhoto;
+  return { data: { ...user, photo: encodedPhoto } };
 };
 
 const update = (id, user) => {
-  userService.update(id, user);
+  const { photo } = user;
+
+  _clearFolder(`${photosStoragePath}/${id}`);
+
+  const fileExtension = photo.slice(parseBase64.data.length, photo.indexOf(parseBase64.base));
+  const fileData = photo.slice(parseBase64.data.length + fileExtension.length + parseBase64.base.length);
+  const filePath = `${photosStoragePath}/${id}/photo.${fileExtension}`;
+
+  fs.writeFileSync(filePath, fileData, 'base64');
+
+  userService.update(id, { ...user, photo: filePath });
 };
 
-module.exports = { getUserInfo, getUserPhoto, update };
+module.exports = { getUserInfo, update };
