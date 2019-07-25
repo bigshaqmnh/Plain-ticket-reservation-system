524145f1c150aa0e75013dc0ae489617f8131fca
mike_kotikov <kmikecoder@gmail.com>
2019-07-25 16:35:23 +0300

    refactor fs operations using fsPromise

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,9 +9,9 @@ const parseBase64 = {
   base: ';base64'
 };
 
-const _clearFolder = folderName => {
-  const files = fs.readdirSync(folderName);
-  files.forEach(file => fs.unlinkSync(path.join(folderName, file)));
+const _clearFolder = async folderName => {
+  const files = await fsPromises.readdir(folderName);
+  files.forEach(async file => await fsPromises.unlink(path.join(folderName, file)));
 };
 
 const getUserInfo = async userId => {
@@ -30,16 +30,16 @@ const getUserInfo = async userId => {
   return { data: { ...user, photo: encodedPhoto } };
 };
 
-const update = (id, user) => {
+const update = async (id, user) => {
   const { photo } = user;
 
-  _clearFolder(`${photosStoragePath}/${id}`);
+  await _clearFolder(`${photosStoragePath}/${id}`);
 
   const fileExtension = photo.slice(parseBase64.data.length, photo.indexOf(parseBase64.base));
   const fileData = photo.slice(parseBase64.data.length + fileExtension.length + parseBase64.base.length);
   const photoFilePath = `${photosStoragePath}/${id}/photo.${fileExtension}`;
 
-  fs.writeFileSync(photoFilePath, fileData, 'base64');
+  await fsPromises.writeFile(photoFilePath, fileData, 'base64');
 
   userService.update(id, { ...user, photo: photoFilePath });
 };
