efae2f9702ff1257fcc80826fed469e82af7965b
mike_kotikov <kmikecoder@gmail.com>
2019-07-31 17:17:04 +0300

    refactor fetch data to check for user token

--- admin/src/api/fetchData.js
+++ admin/src/api/fetchData.js
@@ -7,11 +7,20 @@ const api = axios.create({
   timeout: config.requestTimeout // 30 seconds timeout
 });
 
-const fetchData = ({ needAuth, ...requestParams }) =>
-  api(
+const fetchData = async ({ needAuth, ...requestParams }) => {
+  const userToken = getUserToken();
+
+  if (needAuth && !userToken) {
+    return {};
+  }
+
+  const response = await api(
     needAuth
-      ? { ...requestParams, headers: { ...requestParams.headers, Authorization: `Bearer ${getUserToken()}` } }
+      ? { ...requestParams, headers: { ...requestParams.headers, Authorization: `Bearer ${userToken}` } }
       : requestParams
   );
+
+  return response;
+};
 
 export default fetchData;
