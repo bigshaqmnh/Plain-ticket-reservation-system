a092bcf72177ad8af0dbc6ca1de355f1e36d717c
mike_kotikov <kmikecoder@gmail.com>
2019-08-02 13:08:10 +0300

    get rid of commented code

--- admin/src/api/fetchData.js
+++ admin/src/api/fetchData.js
@@ -7,22 +7,6 @@ const api = axios.create({
   timeout: config.requestTimeout // 30 seconds timeout
 });
 
-// const fetchData = async ({ needAuth, ...requestParams }) => {
-//   const userToken = getUserToken();
-
-//   if (needAuth && !userToken) {
-//     return {};
-//   }
-
-//   const response = await api(
-//     needAuth
-//       ? { ...requestParams, headers: { ...requestParams.headers, Authorization: `Bearer ${userToken}` } }
-//       : requestParams
-//   );
-
-//   return response;
-// };
-
 const fetchData = ({ needAuth, ...requestParams }) =>
   api(
     needAuth
