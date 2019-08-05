bf7f54fdd7fc5bb3de4fe2504f3a034f19a6a94b
mike_kotikov <kmikecoder@gmail.com>
2019-08-05 17:27:45 +0300

    create main screen form

--- client/src/screens/main/index.tsx
+++ client/src/screens/main/index.tsx
@@ -2,16 +2,45 @@ import * as React from 'react';
 
 import InputAutoComplete from './components/InputAutoComplete';
 
-function MainScreen() {
-  return (
-    <InputAutoComplete
-      label="Departure Airport"
-      name="departureAirport"
-      placeholder="Choose departure airport"
-      required
-      onChange={({ currentTarget }: React.FormEvent<HTMLInputElement>) => console.log('value: ', currentTarget.value)}
-    />
-  );
+import { mainFormData } from '../../constants/formDataSchemes';
+
+import { IMainScreenProps, IMainScreenState } from './interface';
+
+class MainScreen extends React.PureComponent<IMainScreenProps, IMainScreenState> {
+  public constructor(props: IMainScreenProps) {
+    super(props);
+    this.state = {
+      formData: mainFormData
+    };
+  }
+
+  private handleChange = ({ currentTarget }: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = currentTarget;
+
+    this.setState((prevState: IMainScreenState) => ({
+      ...prevState,
+      formData: { ...prevState.formData, [name]: { ...prevState.formData[name], value } }
+    }));
+  }
+
+  public render() {
+    const formFields: string[] = Object.keys(this.state.formData);
+
+    return (
+      formFields.map((field) => {
+        const { formData } = this.state;
+
+
+        return <InputAutoComplete
+          label={formData[field].label}
+          name={field}
+          value={formData[field].value}
+          placeholder={formData[field].placeholder}
+          onChange={this.handleChange}
+        />;
+      })
+    );
+  }
 }
 
 export default MainScreen;
