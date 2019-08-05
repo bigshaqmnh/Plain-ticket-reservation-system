8c88003a1b459e656db8f2507ac4e5532ad7d4a7
mike_kotikov <kmikecoder@gmail.com>
2019-08-05 17:24:31 +0300

    connect react router to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
+
 import App from './app';
 
-render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
