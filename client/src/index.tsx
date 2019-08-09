9d3832a7fb12def22e86b82e652212ec2220471c
mike_kotikov <kmikecoder@gmail.com>
2019-08-09 17:11:24 +0300

    connect moment to context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
+import MomentUtils from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 import App from './app';
 
@@ -9,7 +11,9 @@ const history = createBrowserHistory();
 
 render(
   <Router history={history}>
-    <App />
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <App />
+    </MuiPickersUtilsProvider>
   </Router>,
   document.getElementById('root')
 );
