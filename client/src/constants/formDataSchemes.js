68e362a4c3ffe10f12d7b2022b7632feecc2718e
mike_kotikov <kmikecoder@gmail.com>
2019-08-09 17:18:38 +0300

    get rid of unused files

--- client/src/constants/formDataSchemes.js
+++
@@ -1,7 +0,0 @@
-export const mainFormData = {
-  from: { label: 'From', placeholder: 'Departure Airport', value: '' },
-  to: { label: 'To', placeholder: 'Destination Airport', value: '' },
-  flyOut: { label: 'Fly out', placeholder: 'DD/MM/YYYY', value: new Date() },
-  flyBack: { label: 'Fly back', placeholder: 'DD/MM/YYYY', value: new Date() },
-  ammountOfPassengers: { label: 'Passengers', placeholder: 'Ammount of passengers', value: 1 }
-};
