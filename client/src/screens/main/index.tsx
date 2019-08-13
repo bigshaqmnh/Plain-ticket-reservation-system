532f1d24f2e8fae860b7e883f61f92d075686749
mike_kotikov <kmikecoder@gmail.com>
2019-08-13 18:04:49 +0300

    refactor main screen component and add styles

--- client/src/screens/main/index.tsx
+++ client/src/screens/main/index.tsx
@@ -1,69 +1,13 @@
 import * as React from 'react';
-
-import {
-  DateTimePicker
-} from '@material-ui/pickers';
-
-import MainScreenForm from '../../classes/MainScreenForm';
-
-import { mainScreenFormData } from '../../constants/formData';
+import Grid from '@material-ui/core/Grid';
 
 import MainScreenFormComponent from './components/MainScreenForm';
 
-import { IMainScreenProps, IMainScreenState } from './interface';
+import './style.scss';
 
-import { Moment } from 'moment';
-
-class MainScreen extends React.PureComponent<IMainScreenProps, IMainScreenState> {
-  public constructor(props: IMainScreenProps) {
-    super(props);
-    const formData = new MainScreenForm();
-
-    this.state = {
-      formData
-    };
-  }
-
-
-  private getDateHandler = (name: string) => (date: Moment) => {
-    this.setState((prevState) => ({
-      ...prevState,
-      formData: { ...prevState.formData, [name]: { ...prevState.formData[name], date } }
-    }));
-  }
-
-  private DateTimePicker = (props: any) => {
-    const {
-      input: { name, onChange, value, ...restInput },
-      meta,
-      ...rest
-    } = props;
-
-    const showError =
-      ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) &&
-      meta.touched;
-
-    return <DateTimePicker
-      variant="inline"
-      value={value}
-      onChange={this.getDateHandler(name)}
-      showTodayButton={true}
-      format="yyyy/MM/dd HH:mm"
-      error={showError}
-    />
-  }
-
-  private handleDateChange = (date: Date) => console.log('recieved date: ', date);
-
-  public render(): React.ReactNode {
-    const { formData } = this.state;
-
-    return <MainScreenFormComponent
-      {...formData}
-      handleDateChange={this.handleDateChange}
-    />;
-
-  }
-}
+const MainScreen = (): JSX.Element =>
+  <Grid container className="container">
+    <MainScreenFormComponent />
+  </Grid>;
 
 export default MainScreen;
