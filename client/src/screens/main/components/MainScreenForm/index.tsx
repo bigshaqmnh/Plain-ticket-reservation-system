285a5e186135d8b9c9b0a280bd27be5fc24bff18
mike_kotikov <kmikecoder@gmail.com>
2019-08-13 18:06:33 +0300

    fix main screen form component and add styles

--- client/src/screens/main/components/MainScreenForm/index.tsx
+++ client/src/screens/main/components/MainScreenForm/index.tsx
@@ -1,85 +1,119 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import * as Yup from 'yup';
-import { withFormik, FormikProps, FormikErrors, Form, Field } from 'formik';
-import moment = require('moment');
+import { withFormik, FormikProps, Form } from 'formik';
+import { Moment } from 'moment';
 
 import { mainScreenFormData } from '../../../../constants/formData';
 import Input from '../../../../components/Input';
 
-interface FormValues {
+import { DatePicker } from '@material-ui/pickers';
+import moment = require('moment');
+
+import './style.scss';
+
+interface IFormValues {
   from: string;
   to: string;
-  flyOut: Date;
-  flyBack: Date;
+  flyOut: Moment;
+  flyBack: Moment;
   ammountOfPassengers: number;
 }
 
-interface IFormValues<FormValues> {
-  [key: string]: FormValues;
-}
-
-interface IOtherProps {
-  handleDateChange: (date: Date) => void;
-}
-
-function InnerForm(props: IOtherProps & FormikProps<IFormValues<string>>) {
-  const { values, touched, errors, isSubmitting, isValid, handleChange, handleDateChange } = props;
-  console.log('touch: ', touched);
+const InnerForm = (props: FormikProps<IFormValues>) => {
+  const { values, setValues, touched, errors, handleChange, handleBlur, isSubmitting, isValid } = props;
 
   const fields: string[] = Object.keys(values);
 
   return (
-    <Form>
-      {fields.map((field) =>
-        <Field
-          key={field}
+    <Form >
+      <Grid container spacing={8} className="main-form">
+        {fields.map((field: string) => {
+          let component: JSX.Element = null;
 
-          render={() =>
-            <Input
-              name={field}
-              label={mainScreenFormData[field].label}
-              value={values[field]}
-              placeholder={mainScreenFormData[field].placeholder}
-              onChange={handleChange}
-              error={!!errors[field]}
-              helperText={errors[field] || ''}
-            />}
-        />)}
+          if ((field === 'flyOut') || (field === 'flyBack')) {
+            const handleDateChange = (date: Moment) => setValues({ ...values, [field]: date });
 
-      <button type="submit" disabled={isSubmitting || !isValid}>
-        Search
-      </button>
+            component =
+              <DatePicker
+                name={field}
+                className="main-form-date-picker"
+                label={mainScreenFormData[field].label}
+                variant="inline"
+                value={values[field]}
+                onChange={handleDateChange}
+                format="DD-MM-YYYY"
+                autoOk={true}
+                minDate={field === 'flyOut' ? moment() : values.flyOut}
+                disablePast
+                disabled={field === 'flyBack' && !values.flyOut}
+              />;
+          } else {
+            component =
+              <Input
+                name={field}
+                className="main-form-input"
+                label={mainScreenFormData[field].label}
+                value={values[field]}
+                placeholder={mainScreenFormData[field].placeholder}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched[field] && !!errors[field]}
+                helperText={touched[field] ? errors[field] : ''}
+              />;
+          }
+
+          return (
+            <Grid key={field} item className="main-form-item">
+              {component}
+            </Grid>
+          );
+        })}
+
+        <Button
+          type="submit"
+          className="main-form-btn"
+          disabled={isSubmitting || !isValid}
+        >
+          Find flights
+        </Button>
+      </Grid>
     </Form>
   );
 }
 
 interface IFormProps {
-  from: string;
-  to: string;
-  flyOut: Date;
-  flyBack: Date;
-  ammountOfPassengers: number;
-  handleDateChange: (date: Date) => void;
+  from?: string;
+  to?: string;
+  flyOut?: Moment;
+  flyBack?: Moment;
+  ammountOfPassengers?: number;
 }
 
-const MainScreenForm = withFormik<IFormProps, IFormValues<string>>({
-  // mapPropsToValues: (props) => ({
-  //   from: props.from,
-  //   to: props.to,
-  //   flyOut: props.flyOut,
-  //   flyBack: props.flyBack,
-  //   ammountOfPassengers: props.ammountOfPassengers
-  // }),
-
-  validationSchema: Yup.object().shape({
-    from: Yup.string().required('Departure airport is required'),
-    to: Yup.string().required('Destination airport is required'),
-    ammountOfPassengers: Yup.number().required('Ammount of Passengers is required')
+const MainScreenFormComponent = withFormik<IFormProps, IFormValues>({
+  mapPropsToValues: () => ({
+    from: mainScreenFormData.from.initValue,
+    to: mainScreenFormData.to.initValue,
+    flyOut: mainScreenFormData.flyOut.initValue,
+    flyBack: mainScreenFormData.flyBack.initValue,
+    ammountOfPassengers: mainScreenFormData.ammountOfPassengers.initValue,
   }),
 
-  handleSubmit: (values: IFormValues<string>) => {
+  validationSchema: Yup.object().shape({
+    from: Yup.string()
+      .required('Departure airport is required'),
+    to: Yup.string()
+      .required('Destination airport is required'),
+    ammountOfPassengers: Yup.number()
+      .typeError('Only positive numbers are possible')
+      .min(1, 'Must be a positive number')
+      .required('Ammount of Passengers is required')
+  }),
+
+  handleSubmit: (values: IFormValues) => {
     console.log('form values: ', values);
   }
 })(InnerForm);
 
-export default MainScreenForm;
+export default MainScreenFormComponent;
