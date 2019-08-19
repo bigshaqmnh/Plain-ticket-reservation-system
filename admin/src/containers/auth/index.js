cdcb6732b0e1529a8ddccaf3b476a8106324a553
mike_kotikov <kmikecoder@gmail.com>
2019-08-19 11:11:09 +0300

    fix app crash on no token

--- admin/src/containers/auth/index.js
+++ admin/src/containers/auth/index.js
@@ -38,6 +38,7 @@ function AuthContainer(props) {
     props,
     formDataScheme: authFormData,
     validationScheme: authValidationScheme,
+    api: userApi,
     setAlert,
     setShowAlert
   });
