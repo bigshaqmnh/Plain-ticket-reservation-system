770a8b3463407ffc8e02da034f01366356ea96ff
mike_kotikov <kmikecoder@gmail.com>
2019-08-21 13:54:58 +0300

    refactor variable names

--- client/src/constants/formData.ts
+++ client/src/constants/formData.ts
@@ -1,17 +1,17 @@
-interface MainScreenFormData {
+interface IFormData {
   initValue: any;
   label: string;
   placeholder: string;
 }
 
-interface IMainScreenFormData<MainScreenFormData> {
-  [key: string]: MainScreenFormData;
+interface IMainScreenFormData<IFormData> {
+  [key: string]: IFormData;
 }
 
-export const mainScreenFormData: IMainScreenFormData<MainScreenFormData> = {
-  from: { initValue: '', label: 'From', placeholder: 'Departure airport' },
-  to: { initValue: '', label: 'To', placeholder: 'Destination airport' },
+export const mainScreenFormData: IMainScreenFormData<IFormData> = {
+  from: { initValue: '', label: 'From', placeholder: 'Country, city' },
+  to: { initValue: '', label: 'To', placeholder: 'Country, city' },
   flyOut: { initValue: null, label: 'Fly out', placeholder: 'DD/MM/YYYY' },
   flyBack: { initValue: null, label: 'Fly back', placeholder: 'DD/MM/YYYY' },
-  ammountOfPassengers: { initValue: 1, label: 'Passengers', placeholder: 'Enter ammount of passengers' }
+  amountOfPassengers: { initValue: 1, label: 'Passengers', placeholder: '1' }
 };
