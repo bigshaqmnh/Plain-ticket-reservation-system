ee370712de2f0966d9edc37d1e2aaa0dfd05616b
mike_kotikov <kmikecoder@gmail.com>
2019-08-26 11:19:38 +0300

    create necessary interfaces

--- client/src/screens/main/interface.ts
+++ client/src/screens/main/interface.ts
@@ -1,5 +1,64 @@
-export interface IMainScreenProps { }
+import { Moment } from 'moment';
 
-export interface IMainScreenState {
-  [key: string]: any;
+export interface IAirport {
+  id: number;
+  name: string;
+  country?: string;
+  city?: string;
+}
+
+export interface IAirplane {
+  id: number;
+  name: string;
+  type?: string;
+  maxLuggageCarryWeight?: number;
+}
+
+export interface IFlight {
+  id: number;
+  departureTime: Moment;
+  arrivalTime: Moment;
+  luggageOverweightCost: string;
+  isCancelled: boolean;
+  airplaneId: number;
+  departureAirport: IAirport;
+  arrivalAirport: IAirport;
+  airplane: IAirplane;
+}
+
+export interface IFlightFetchRequest {
+  depCountry: string;
+  depCity: string;
+  arrCountry: string;
+  arrCity: string;
+  departureTime: string;
+  amountOfPassengers: number;
+  page?: number;
+  limit?: number;
+}
+
+export interface IAirportData {
+  [key: string]: IAirport[];
+}
+
+export interface IFlightData {
+  data: IFlight[];
+}
+
+export interface IFlightState {
+  forward?: IFlight[];
+  backward?: IFlight[];
+}
+
+export interface IAction {
+  type: string;
+  payload: any;
+}
+
+export interface IInitialState {
+  fetchParams?: IFlightFetchRequest;
+  isFetching: boolean;
+  data: IAirportData | IFlightState;
+  selectedItem?: number;
+  error: Error;
 }
