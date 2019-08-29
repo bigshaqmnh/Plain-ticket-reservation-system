2bc69e67bb9177ead18b798fde27f6359b4ed5d1
mike_kotikov <kmikecoder@gmail.com>
2019-08-29 19:04:16 +0300

    add ticket svg icon

--- client/src/modules.d.ts
+++ client/src/modules.d.ts
@@ -3,6 +3,11 @@ declare module '*.jpg' {
   export = value;
 }
 
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
 declare module '*.scss' {
   const styles: { [className: string]: string };
   export default styles;
