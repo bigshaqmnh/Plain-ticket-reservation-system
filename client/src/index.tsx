0aefd2c2d87acf7736829fcb8cec588826fcbe12
mike_kotikov <kmikecoder@gmail.com>
2019-08-30 15:34:36 +0300

    move history to separete file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import MomentUtils from '@date-io/moment';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 import App from './app';
 
-const history = createBrowserHistory();
+import history from './history';
 
 render(
   <Router history={history}>
     <MuiPickersUtilsProvider utils={MomentUtils}>
-      <App />
+      <App/>
     </MuiPickersUtilsProvider>
   </Router>,
   document.getElementById('root')
