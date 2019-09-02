9b4ccf1ed493ff1ea759a4f71939b6e210548d6d
mike_kotikov <kmikecoder@gmail.com>
2019-09-02 10:50:17 +0300

    add module declaration for .png files

--- client/src/modules.d.ts
+++ client/src/modules.d.ts
@@ -3,7 +3,12 @@ declare module '*.jpg' {
   export = value;
 }
 
-declare module "*.svg" {
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.svg' {
   const content: any;
   export default content;
 }
