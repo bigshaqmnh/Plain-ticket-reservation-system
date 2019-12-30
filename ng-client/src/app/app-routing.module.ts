0a923f40012eb996d4885bb5ef8409de8b7fa60d
Nikita <nikita@hellonoken.com>
2019-12-30 18:20:41 +0300

    ENG-1: add routing to main page

--- ng-client/src/app/app-routing.module.ts
+++ ng-client/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TicketsSearchFormComponent } from './components/TicketsSearchForm/TicketsSearchForm.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TicketsSearchFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
