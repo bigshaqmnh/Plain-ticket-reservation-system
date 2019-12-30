2f6f2616bf15cf2b1a4d4d9e9fe1c50c8e9eb1c3
Nikita <nikita@hellonoken.com>
2019-12-30 18:15:48 +0300

    ENG-1: create mock for http requests

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpMockRequestInterceptor } from './helpers/HttpMockRequestInterceptor';
+
+import { environment } from '../environments/environment';
+
+const isMock = environment.mock;
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: isMock ? HttpMockRequestInterceptor : null,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
