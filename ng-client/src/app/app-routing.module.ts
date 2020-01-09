c96c9b2a9e6a835388c7ec24c28e84557d171320
Nikita <nikita@hellonoken.com>
2020-01-09 18:08:47 +0300

    ENG-1: implement routing to not found page

--- ng-client/src/app/app-routing.module.ts
+++ ng-client/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
+import { NotFoundComponent } from './not-found.component';
+
 const routes: Routes = [
   { path: '', loadChildren: './flight/flight.module#FlightModule' },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
